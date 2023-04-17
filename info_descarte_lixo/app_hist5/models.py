cb0f6ef51977fb21ba1e0c02df7b12b6f6e4e5e7
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-04-17 13:49:07 -0300

    Delete info_descarte_lixo directory

--- info_descarte_lixo/app_hist5/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Pergunta(models.Model):
-    id = models.AutoField(primary_key=True)
-    email = models.TextField(max_length=355)
-    texto = models.TextField(max_length=3000)
